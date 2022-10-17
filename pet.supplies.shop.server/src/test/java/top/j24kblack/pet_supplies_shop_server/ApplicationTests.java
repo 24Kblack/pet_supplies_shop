5ad2f55c27fcea843eb3ffde8a016da43b0ebb7a
24kblack <my24kblack@gmail.com>
2022-10-17 23:05:46 +0800

    Initial commit

---
+++ pet.supplies.shop.server/src/test/java/top/j24kblack/pet_supplies_shop_server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.j24kblack.pet_supplies_shop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
